ded93fd81fd3960f8c6b91ca8683e3f43dfb751c
IsraelCasas <isra_rey@hotmail.es>
2015-11-02 14:50:27 -0600

    Listas dobles en c

---
+++ listas/listas_en_c/listasdobles/progra4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "operadoble.h"
+
+int main()
+{
+	struct nodo lista;
+	int valor;
+	int valor_antes;
+
+	inicializar(&lista);
+	printf("Programa en el cual tendras que insertar numeros en lista \n");
+	printf("y termina cuando insertas un -1, y aparte insertaras un numero \n");
+	printf("nuevo y podras insertarlo en el lugar que quieras de la lista. \n\n");
+	while(valor != -1){
+	printf("Escriba un numero \n");
+	
+		scanf("%d", &valor);
+		if(valor != -1)
+		insertar_al_final(&lista,valor);
+	}
+	printf("Escriba el nuevo numero a introducir\n");
+	scanf("%d", &valor);
+	printf("Despues de que numero de la lista quieres que este el numero nuevo\n");
+	imprimir_lista(&lista);
+	printf("\n");
+	scanf("%d", &valor_antes);
+	insertar_despues_de(&lista, valor, valor_antes);
+	imprimir_lista(&lista);
+	
+	return 0;
+}
+
+    
+
---
+++ listas/listas_en_c/listasdobles/progra5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "operadoble.h"
+
+int main ()
+{
+	struct nodo lista;
+	int buscar;
+	int valor = 1;
+	inicializar(&lista);
+	
+	printf("Programa en el cual tendras que insertar numeros en lista \n");
+	printf("y termina insertando un -1, en el cual de  la lista insertada \n");
+	printf("tendras que borrar un numero de ella. \n\n");
+	while(valor != -1){
+	printf("Escriba un numero \n");
+	
+		scanf("%d", &valor);
+		if(valor != -1)
+		insertar_al_final(&lista,valor);
+	}
+	
+	printf("escribe el numero a borrar de la siguiente lista\n");
+	imprimir_lista(&lista);
+	printf("\n");
+	scanf("%d", &buscar);
+	eliminar_nodo(&lista,buscar);
+	imprimir_lista(&lista);
+	borrar_lista(&lista);
+	return 0;
+}
